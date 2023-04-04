cde882629fa072461446e71198bdec19c5fe1ae9
indivar shaik <indivarangular@gmail.com>
2023-04-05 01:00:14 +0530

    Implementation of generating CorrelationId for logs for REST API calls using slf4j MDC

--- src/main/java/com/calculator/calculator/CalculatorApplication.java
+++ src/main/java/com/calculator/calculator/CalculatorApplication.java
@@ -2,12 +2,20 @@ package com.calculator.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@ServletComponentScan
 
 @SpringBootApplication
 public class CalculatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
+		//new SpringApplicationBuilder().main(CalculatorApplication.class).profiles("mdc").run(args);
 		System.out.println("Calculator");
 		System.out.println();
 	}
--- src/main/java/com/calculator/calculator/controller/CalculatorController.java
+++ src/main/java/com/calculator/calculator/controller/CalculatorController.java
@@ -2,12 +2,14 @@ package com.calculator.calculator.controller;
 
 import com.calculator.calculator.entity.Variables;
 import com.calculator.calculator.service.CalculatorService;
+import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class CalculatorController {
 
@@ -15,6 +17,7 @@ public class CalculatorController {
     private CalculatorService calculatorService;
     @PostMapping("/calculate")
     public String getResult(@RequestBody @NotNull Variables variables){
+        log.info("Entered calculator endpoint");
         return "The result is: "+ calculatorService.performCalculation(variables.firstValue, variables.secondValue, variables.flag);
     }
 
